9ff949e375fc5ff2d60d1faceac0396a1d31c573
Chris-rc0807 <rchristianofficial@gmail.com>
2024-10-31 13:19:34 +0000

    modified:   src/aed_formulario_proyectofinal/Formulario.java

--- src/aed_formulario_proyectofinal/Formulario.java
+++ src/aed_formulario_proyectofinal/Formulario.java
@@ -1,12 +1,20 @@
 
 package aed_formulario_proyectofinal;
+import java.util.Scanner;
 
 public class Formulario {
         /**
          * 
          */
     public static void main (String[] args){
-        System.out.println("hello world!");
+        Scanner scan=new Scanner(System.in);
+        int a,b,c;
+        System.out.println("Ingrese número a: ");
+        a=scan.nextInt();
+        System.out.println("Ingrese número b: ");
+        b=scan.nextInt();
+        c=a+b;
+        System.out.println("Resultado: \n"+c);
 
     }
 }
