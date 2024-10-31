43f831db7c4fb7b0f3acd9c6e32dce231bdfe166
Chris-rc0807 <rchristianofficial@gmail.com>
2024-10-31 07:28:37 -0500

    Add files via upload

--- src/aed_formulario_proyectofinal/Formulario.java
+++ src/aed_formulario_proyectofinal/Formulario.java
@@ -2,10 +2,13 @@
 package aed_formulario_proyectofinal;
 
 public class Formulario {
+<<<<<<< HEAD
     public static void main (String[] args){
         /**
          * 
          */
-        
+    public static void main (String[] args){
+        System.out.println("hello world!");
+
     }
 }
